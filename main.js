ff2da33b190ec59a2662825513bb9abb8e004b79
prohorov01 <alexprohorov01@gmail.com>
2023-03-18 20:00:51 +0200

    update number prompt

---
+++ main.js
@@ -0,0 +1,5 @@
+const a = Number(prompt("Перше число", " "));
+const b = Number(prompt("Друге число", " "));
+const c = Number(prompt("Третє число", " "));
+alert((a + b + c) / 3);
+console.log("");
